3104180e515197ee2a9f0c68951d391e143f0478
csantilmuy <csantilmuy@educacion.navarra.es>
2024-11-25 11:55:21 +0100

    1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,3 +2,9 @@
 # {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por una divisa y
 # muestre su símbolo o un mensaje de aviso si la divisa no está en
 # el diccionario.
+diccionario = {"Euro":"€", "Dollar":"$", "Yen":"¥"}
+divisa = input("Escriba una divisa (Euro, dollar, yen): ").capitalize()
+if divisa in diccionario:
+    print("La divisa es: " + diccionario[divisa])
+else:
+    print("Error, la divisa no esta en el diccionario")
