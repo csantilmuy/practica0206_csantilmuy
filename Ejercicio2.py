af990f5c27c21e5585e3816e9e5972a608229e90
csantilmuy <csantilmuy@educacion.navarra.es>
2024-11-25 12:46:05 +0100

    2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,3 +2,12 @@
 # teléfono y lo guarde en un diccionario. Después debe mostrar por pantalla el
 # mensaje “<nombre> tiene <edad> años, vive en <dirección> y su número de
 # teléfono es <teléfono>”.
+nombre = input("Escriba su nombre: ")
+edad = input("Escriba su edad: ")
+direccion = input("Escriba su direccion: ")
+telefono = input("Escriba su telefono: ")
+diccionario = {"nombre": nombre, "edad": edad, "direccion": direccion, 
+               "telefono": telefono}
+print(diccionario["nombre"], "tiene", diccionario["edad"], "años, vive en",
+      diccionario["direccion"], "y su número de teléfono es",
+      diccionario["telefono"])
